1739d853be4a0c3b8cc7804ec4282ccaac06d0ce
KacperStanclik <70938287+Czapa1@users.noreply.github.com>
2023-02-14 09:32:23 +0100

    Added script

---
+++ My project (5)/Assets/Scripts/Player_controler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controler : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement);
+    }
+}
+
