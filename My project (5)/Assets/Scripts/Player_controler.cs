1963e1967705dbc1adaa83538e9530338fa5d7b5
KacperStanclik <70938287+Czapa1@users.noreply.github.com>
2023-02-14 10:07:13 +0100

    Added Camera script

--- My project (5)/Assets/Scripts/Player_controler.cs
+++ My project (5)/Assets/Scripts/Player_controler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_controler : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    public float speed;
     
     void Start()
     {
@@ -21,7 +22,7 @@ public class Player_controler : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement);
+        rb2d.AddForce(movement*speed);
     }
 }
 
